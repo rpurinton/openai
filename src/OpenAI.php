b4c04836c879d3d4c0df486658472c933bbd5cfe
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2025-02-18 17:25:57 -0500

    Updates

--- src/OpenAI.php
+++ src/OpenAI.php
@@ -9,6 +9,7 @@ use OpenAI\Client;
 use RPurinton\Config;
 use RPurinton\Exceptions\ConfigException;
 use RPurinton\Exceptions\OpenAIException;
+use RPurinton\Validators\OpenAIValidators;
 
 /**
  * Class OpenAI
@@ -61,39 +62,10 @@ class OpenAI
 
 		$this->ai = \OpenAI::client($apiKey);
 		$this->encoder = new Encoder();
-		$this->prompt =  $this->validatedPrompt($this->getConfig());
+		$this->prompt =  $this->getConfig();
+		OpenAIValidators::validatePrompt($this->prompt);
 	}
 
-	/**
-	 * Get the validated prompt configuration.
-	 *
-	 * @return array
-	 */
-	public function validatedPrompt(array $prompt): array
-	{
-		$keys = [
-			'model' => 'string',
-			'temperature' => 'float',
-			'top_p' => 'float',
-			'frequency_penalty' => 'float',
-			'presence_penalty' => 'float',
-			'max_tokens' => 'int',
-			'stop' => 'array',
-			'messages' => 'array',
-			'tools' => 'array',
-			'functions' => 'array',
-		];
-		foreach ($prompt as $key => $value) {
-			if (array_key_exists($key, $keys)) {
-				if (gettype($value) !== $keys[$key]) {
-					throw new ConfigException("Invalid type for $key in OpenAI config. Expected $keys[$key], got " . gettype($value));
-				}
-			} else {
-				throw new ConfigException("Invalid key $key in OpenAI config.");
-			}
-		}
-		return $prompt;
-	}
 
 	/**
 	 * Get the prompt configuration.
---
+++ src/Validators/OpenAIValidators.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace RPurinton\Validators;
+
+use RPurinton\Exceptions\ConfigException;
+
+class OpenAIValidators
+{
+    /**
+     * Get the validated prompt configuration.
+     *
+     * @return array
+     */
+    public static function validatePrompt(array $prompt): bool
+    {
+        $keys = [
+            'model' => 'string',
+            'temperature' => 'double',
+            'top_p' => 'double',
+            'frequency_penalty' => 'double',
+            'presence_penalty' => 'double',
+            'max_tokens' => 'int',
+            'stop' => 'array',
+            'messages' => 'array',
+            'tools' => 'array',
+            'functions' => 'array',
+        ];
+        foreach ($prompt as $key => $value) {
+            if (array_key_exists($key, $keys)) {
+                if (gettype($value) !== $keys[$key]) {
+                    throw new ConfigException("Invalid type for '$key' in OpenAI config. Expected {$keys[$key]}, got " . gettype($value));
+                }
+            } else {
+                throw new ConfigException("Invalid key '$key' in OpenAI config.");
+            }
+        }
+        return true;
+    }
+}
