3d192a4f2cf1208ad1fe2cf8898437f77f61e0d5
Russell Purinton <russell.purinton@gmail.com>
2025-03-01 10:56:39 +0000

    Version 1.0.29 - 2025-03-01

--- testOpenAI.php
+++ testOpenAI.php
@@ -5,6 +5,6 @@ require __DIR__ . '/vendor/autoload.php';
 
 use RPurinton\OpenAI;
 
-$ai = OpenAI::connect();
+$ai = OpenAI::connect(getenv('OPENAI_KEY_TEST'));
 echo $ai->ask("What is the capital of the United States?") . PHP_EOL;
 // Response: The capital of the United States is Washington, D.C.
